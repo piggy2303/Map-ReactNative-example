ff82200d36e57250d03fec4c02ac82374145ceda
piggy2303 <nguyentuananh23031997@gmail.com>
2018-05-08 16:15:59 +0700

    simple location

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-import React, { Component } from 'react';
-import {  View, Text,StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import React, { Component } from "react";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
+import MapView from "react-native-maps";
+import { Marker } from "react-native-maps";
+
+const { width, height } = Dimensions.get("window");
+
+const ASPECT_RATIO = width / height;
+const LATITUDE = 21.0166034;
+const LONGITUDE = 105.7892057;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const SPACE = 0;
 
 const styles = StyleSheet.create({
   container: {
@@ -8,32 +18,77 @@ const styles = StyleSheet.create({
     // height: 400,
     // width: 400,
     flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
+    justifyContent: "flex-end",
+    alignItems: "center"
   },
   map: {
-    ...StyleSheet.absoluteFillObject,
-  },
+    ...StyleSheet.absoluteFillObject
+  }
 });
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mapRegion: null,
+      lastLat: null,
+      lastLong: null
+    };
+  }
+  componentDidMount() {
+    this.watchID = navigator.geolocation.watchPosition(position => {
+      // Create the object to update this.state.mapRegion through the onRegionChange function
+      let region = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        latitudeDelta: 0.00922 * 1.5,
+        longitudeDelta: 0.00421 * 1.5
+      };
+      this.onRegionChange(region, region.latitude, region.longitude);
+    });
+  }
+  onRegionChange(region, lastLat, lastLong) {
+    this.setState({
+      mapRegion: region,
+      // If there are no new values set the current ones
+      lastLat: lastLat || this.state.lastLat,
+      lastLong: lastLong || this.state.lastLong
+    });
+  }
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
+  onMapPress(e) {
+    let region = {
+      latitude: e.nativeEvent.coordinate.latitude,
+      longitude: e.nativeEvent.coordinate.longitude,
+      latitudeDelta: 0.00922,
+      longitudeDelta: 0.00421
+    };
+    this.onRegionChange(region, region.latitude, region.longitude);
+  }
   render() {
-    const { region } = this.props;
-    console.log(region);
-
     return (
-      <View style ={styles.container}>
+      <View style={{ flex: 1 }}>
         <MapView
           style={styles.map}
-          region={{
-            // latitude: 37.78825,
-            // longitude: -122.4324,
-            latitude:21.0166034,
-            longitude:105.7892057,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121,
-          }}
+          region={this.state.mapRegion}
+          showsUserLocation={true}
+          followUserLocation={true}
+          // onRegionChange={this.onRegionChange.bind(this)}
         >
+          <MapView.Marker
+            coordinate={{
+              latitude: this.state.lastLat + 0.0005 || -36.82339,
+              longitude: this.state.lastLong + 0.0005 || -73.03569
+            }}
+          >
+            <View>
+              <Text style={{ color: "#000" }}>
+                {this.state.lastLong} / {this.state.lastLat}
+              </Text>
+            </View>
+          </MapView.Marker>
         </MapView>
       </View>
     );
