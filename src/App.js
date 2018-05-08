061cfc8d7406ab0a99353ad3e29d655b14f60b48
piggy2303 <nguyentuananh23031997@gmail.com>
2018-05-08 14:53:52 +0700

    simple example

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import {  View, Text,StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    // height: 400,
+    // width: 400,
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
+
+export default class App extends Component {
+  render() {
+    const { region } = this.props;
+    console.log(region);
+
+    return (
+      <View style ={styles.container}>
+        <MapView
+          style={styles.map}
+          region={{
+            // latitude: 37.78825,
+            // longitude: -122.4324,
+            latitude:21.0166034,
+            longitude:105.7892057,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+        >
+        </MapView>
+      </View>
+    );
+  }
+}
